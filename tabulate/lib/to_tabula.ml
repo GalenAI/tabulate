3bfabfc412d3491ab00d313e99e5e0407608e18d
Edward Li <edwardli@andrew.cmu.edu>
2022-10-02 03:56:18 -0400

    simplify to_tabula

--- tabulate/lib/to_tabula.ml
+++ tabulate/lib/to_tabula.ml
@@ -1,5 +1,4 @@
 open Core
-open Async
 
 module Control = struct
   type t = Proceed | Dismiss [@@deriving sexp, yojson]
@@ -14,19 +13,16 @@ type t =
 let%expect_test "transcription" =
   [%yojson_of: t] (Transcription "testing 1 2 3")
   |> Yojson.Safe.to_string |> print_endline;
-  [%expect {| ["Transcription","testing 1 2 3"] |}];
-  Deferred.unit
+  [%expect {| ["Transcription","testing 1 2 3"] |}]
 
 let%expect_test "control" =
   [%yojson_of: t] (Control Proceed) |> Yojson.Safe.to_string |> print_endline;
   [%expect {| ["Control",["Proceed"]] |}];
   [%yojson_of: t] (Control Dismiss) |> Yojson.Safe.to_string |> print_endline;
-  [%expect {| ["Control",["Dismiss"]] |}];
-  Deferred.unit
+  [%expect {| ["Control",["Dismiss"]] |}]
 
 let%expect_test "mistakes" =
   [%yojson_of: t]
     (Mistakes [ Mistakes.Drug_to_drug { first = "a"; second = "b" } ])
   |> Yojson.Safe.to_string |> print_endline;
-  [%expect {| ["Mistakes",[["Drug_to_drug",{"first":"a","second":"b"}]]] |}];
-  Deferred.unit
+  [%expect {| ["Mistakes",[["Drug_to_drug",{"first":"a","second":"b"}]]] |}]
