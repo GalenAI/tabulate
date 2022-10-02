0d24bdcc96bb941d14138166992568e3b25eaa4a
Edward Li <edwardyaoli@gmail.com>
2022-10-01 23:15:01 -0400

    JSON examples for mistakes

--- tabulate/lib/command.ml
+++ tabulate/lib/command.ml
@@ -13,6 +13,22 @@ module Mistakes = struct
 
   include T
   include Comparable.Make (T)
+
+  let%expect_test "empty list" =
+    [%yojson_of: t list] [] |> Yojson.Safe.to_string |> print_endline;
+    [%expect {| [] |}]
+
+  let%expect_test "all possible" =
+    [%yojson_of: t list]
+      [
+        Drug_to_drug { first = "a"; second = "b" };
+        Drug_allergy "test";
+        Wrong_side;
+        Wrong_site { actual = "a"; wrong = "b" };
+      ]
+    |> Yojson.Safe.to_string |> print_endline;
+    [%expect
+      {| [["Drug_to_drug",{"first":"a","second":"b"}],["Drug_allergy","test"],["Wrong_side"],["Wrong_site",{"actual":"a","wrong":"b"}]] |}]
 end
 
 type t = {
