1bfb057bdf222a912ac8e620f78cbd0f65e3e2f9
Edward Li <edwardyaoli@gmail.com>
2022-10-02 03:04:17 -0400

    add websocket bridge

---
+++ zmq_websocket_bridge.py
@@ -0,0 +1,24 @@
+import asyncio
+import zmq
+import websockets
+
+async def bridge(websocket):
+    ctx = zmq.Context()
+    socket = ctx.socket(zmq.SUB)
+    socket.connect('ipc:///tmp/tabula')
+    socket.subscribe('')
+
+    while True:
+        message = socket.recv()
+        await websocket.send(message)
+
+async def main():
+    try:
+        async with websockets.serve(bridge, host='0.0.0.0', port=6969, max_size=999999999):
+            await asyncio.Future()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(main()))
